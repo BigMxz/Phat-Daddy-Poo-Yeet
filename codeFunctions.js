880a2559e936d0968b4eb413bde6751d6500bd45
PhatDaddyYeet <44023274+PhatDaddyYeet@users.noreply.github.com>
2018-10-10 19:49:57 +0100

    Update codeFunctions.js

--- codeFunctions.js
+++ codeFunctions.js
@@ -1,14 +1,17 @@
 var tot = 0
-//var rolled = false
+var rolled = false
 function rollDice() {
   return Math.floor(Math.random() * (7 - 1) ) + 1;
 }
 function output() {
-  //if (rolled == false) {
+  if (rolled == false) {
     var x = rollDice();
     tot = tot + x
     document.getElementById("out").innerHTML = x;
     document.getElementById("addsup").innerHTML = tot;
-    //rolled = true
+    rolled = true
 }
+}
+function reset() {
+  rolled = false
 }
