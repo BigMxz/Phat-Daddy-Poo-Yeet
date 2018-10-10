a403dbbf4a81fcfee0f3cb7656f880baf226fd3a
PhatDaddyYeet <44023274+PhatDaddyYeet@users.noreply.github.com>
2018-10-10 12:24:05 +0100

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -3,9 +3,8 @@ function rollDice() {
 }
 function output() {
   var x = rollDice();
-  document.getElementById("out").innerHTML = x;
-}
-function outTot() {
   var tot = 0
   tot = tot + x
+  document.getElementById("out").innerHTML = x;
+  document.getElementById("addsup").innerHTML = tot;
 }
