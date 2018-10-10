9cfa41eadd8fd655125343ac685a09bcdef69475
PhatDaddyYeet <44023274+PhatDaddyYeet@users.noreply.github.com>
2018-10-10 12:29:09 +0100

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,9 +1,9 @@
+var tot = 0
 function rollDice() {
   return Math.floor(Math.random() * (7 - 1) ) + 1;
 }
 function output() {
   var x = rollDice();
-  var tot = 0
   tot = tot + x
   document.getElementById("out").innerHTML = x;
   document.getElementById("addsup").innerHTML = tot;
